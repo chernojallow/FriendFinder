af6972811fa5223a4db8961643a6cc85a872020d
Zika Chea <jallsadou@gmail.com>
2018-09-05 20:59:11 -0500

    Adding

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,8 +7,34 @@ module.exports = function(app){
      res.json(friendData);
     });
 
-    app.post("api/friends", function(req, res){
+    app.post("/api/friends", function(req, res){
        
+        var input = req.body;
+      //  console.log('input',  input);
+        console.log("=============================");
+        var userInput = input.scores;
+       // console.log("userresponses",  userResponses);
+
+       // Loop through all friendData
+       for (var i = 0; i < friendData.length; i++){
+         
+        var difference = 0;
+
+        //Compare difference for each question
+       for( var j = 0; i < userInput.length; j++){
+           difference += Math.abs(friendData[i].scores[j] - userInput[j]);
+          //console.log("userinput", userInput[j]);
+       }
+       console.log("difference" + difference);
+    }
+     
+      if (friendData.length < 50) {
+             friendData.push(input);
+          res.json(true);
+       } 
+       else{
+           console.log("Is full");
+      }
 
     });
-}
+};
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,13 +6,13 @@ var path = require ("path");
 module.exports = function(app) {
     //HTML GET request 
     app.get("/survey", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
+       res.sendFile(path.join(__dirname, "../public/survey.html"));
 
     });
 
     // If no matching route is found default to home
     app.get("*", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+   res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
-}
+};
